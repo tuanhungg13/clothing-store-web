66bfad64ed3a93a35de67c9e379a280121343b9b
hwglee <hung1306.lth@gmail.com>
2025-04-15 00:19:26 +0700

    fix role

--- src/app/admin/products/page.js
+++ src/app/admin/products/page.js
@@ -162,6 +162,7 @@ export default function ProductManage() {
                         fetchProducts={fetchProducts}
                         data={dataProduct}
                         isOpen={isOpenModal}
+                        loading={isLoading}
                     />
                 </Spin>
             </Drawer>
